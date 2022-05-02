fc2b17371d7d91af72ae9591943fcfd81f060045
Yuta Lovan <yhlovan@gmail.com>
2022-05-02 10:59:37 -0700

    created docker compose configuration and migration sql script

--- src/obj/Debug/net6.0/StrengthifyNETAPI.AssemblyInfo.cs
+++ src/obj/Debug/net6.0/StrengthifyNETAPI.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("2c6a62a4c5d9b18a9774d5eaf78f5d0e")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("StrengthifyNETAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
